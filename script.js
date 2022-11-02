f6307a01489854714fdac0ea1ff27f1caba46a99
CarbGoblin <bfairer90@gmail.com>
2022-11-02 15:37:04 -0400

    Finish playRound function and its inputs

--- script.js
+++ script.js
@@ -14,19 +14,45 @@
 //else if userSelection === paper && compuerSelection === paper then tie
 //else if userSelection === paper && computerSelection === scissors then computer wins
 const choices = [
-    "Rock",
-    "Paper",
-    "Scissors"
+    "rock",
+    "paper",
+    "scissors"
 ];
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
+const computerChoice = getComputerChoice();
+console.log("Computer chose " + `${computerChoice}` + "!");
 
-let playerChoice = prompt("Choose Rock, Paper, or Scissors")
-function playRound(playerChoice, computerChoice) {
-
+const playerChoice = prompt("Choose Rock, Paper, or Scissors").toLocaleLowerCase();
+if (playerChoice === "paper" || playerChoice === "scissors" || playerChoice === "rock") {
+    console.log("You chose " + `${playerChoice}` + "!");
+} else {
+    console.log("Error! Try again!");
 }
+
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "rock") {
+        console.log("Tie!")
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("Computer Wins!")
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("Player Wins!")
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("Player Wins!")
+    } else if (playerChoice === "paper" && computerChoice === "paper") {
+        console.log("Tie!")
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("Computer Wins!")
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("Computer Wins!")
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("Player Wins!")
+    } else if (playerChoice === "scissors" && computerChoice === "scissors") {
+        console.log("Tie!")
+    }
+}
+
+playRound();
